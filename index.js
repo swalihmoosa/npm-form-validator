586853661ce0e5c3939b79a8bc3ff221b9406d29
swalih_moosa <swalih@steyp.com>
2023-04-01 21:46:28 +0530

    index.js ongoing

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
-function formValidator(string) {
-    return string === "swalih";
+function formValidator() {
+    const validationRules = {
+        name: {
+            value: "",
+            required: true,
+            type: "text",
+            minLength: 4,
+            maxLength: 10,
+        },
+    };
+
+    const validateStates = () => {
+        return Object.keys(validationRules).map((key) => {
+            if (
+                validationRules[key]?.required === true &&
+                validationRules[key]?.value === ""
+            ) {
+                return `${key} is required`;
+            }
+        });
+    };
+
+    // validateStates();
+    console.log(validateStates());
 }
 
+formValidator();
 module.exports = formValidator;
