67af4605e6285672a3977aeffefa5ba6f6b18509
swalih_moosa <swalih@steyp.com>
2023-04-02 18:30:03 +0530

    condition for type = number | completed

--- index.js
+++ index.js
@@ -1,28 +1,82 @@
-function formValidator() {
-    const validationRules = {
-        name: {
-            value: "",
-            required: true,
-            type: "text",
-            minLength: 4,
-            maxLength: 10,
-        },
-    };
+function formValidator(validationRules) {
+    const hasSpecialChars = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/;
+    const hasUppercase = /[A-Z]/;
+    const hasDigit = /\d/;
+    const isDigit = /^\d+$/;
 
     const validateStates = () => {
-        return Object.keys(validationRules).map((key) => {
-            if (
-                validationRules[key]?.required === true &&
-                validationRules[key]?.value === ""
-            ) {
-                return `${key} is required`;
-            }
-        });
+        const errors = [];
+
+        if (validationRules) {
+            Object.keys(validationRules).map((key) => {
+                // condition for checking required or not
+                if (
+                    validationRules[key]?.required === true &&
+                    validationRules[key]?.value === ""
+                ) {
+                    errors.push(`${key} is required`);
+                }
+                // condition for checking minimum length
+                else if (
+                    validationRules[key]?.required === true &&
+                    validationRules[key]?.value?.toString()?.length <
+                        validationRules[key]?.minLength
+                ) {
+                    errors.push(
+                        `minimum ${validationRules[key]?.minLength} character is required for ${key}`
+                    );
+                }
+                // condition for checking maximum length
+                else if (
+                    validationRules[key]?.value?.toString()?.length >
+                    validationRules[key]?.maxLength
+                ) {
+                    errors.push(
+                        `maximum length is ${validationRules[key]?.maxLength} for ${key}`
+                    );
+                }
+                // condition for checking type = text
+                else if (
+                    validationRules[key]?.type === "text" &&
+                    hasSpecialChars.test(validationRules[key]?.value)
+                ) {
+                    errors.push(`special character not allowed in ${key}`);
+                }
+                // condition for checking type = password
+                else if (validationRules[key]?.type === "password") {
+                    // condition for checking uppercase is present or not
+                    if (!hasUppercase.test(validationRules[key]?.value)) {
+                        errors.push(
+                            `${key} should contain atleast one uppercase`
+                        );
+                    }
+                    // condition for checking digit is present or not
+                    else if (!hasDigit.test(validationRules[key]?.value)) {
+                        errors.push(`${key} should contain atleast one number`);
+                    }
+                    // condition for checking special character is present or not
+                    else if (
+                        !hasSpecialChars.test(validationRules[key]?.value)
+                    ) {
+                        errors.push(
+                            `${key} should contain atleast one special character`
+                        );
+                    }
+                }
+                // condition for checking type = number
+                else if (
+                    validationRules[key]?.type === "number" &&
+                    !isDigit.test(validationRules[key]?.value)
+                ) {
+                    errors.push(`${key} should contain only numbers`);
+                }
+            });
+        }
+
+        return errors;
     };
 
-    // validateStates();
-    console.log(validateStates());
+    return validateStates();
 }
 
-formValidator();
 module.exports = formValidator;
--- test.js
+++
@@ -1,8 +0,0 @@
-validationRules = {
-    name: {
-        required: true,
-        type: "text",
-        minLength: 4,
-        maxLength: 10,
-    },
-};
