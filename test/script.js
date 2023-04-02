4671acfe618f3c060e61df48c614ca26d6eb1bf9
swalih_moosa <swalih@steyp.com>
2023-04-02 12:34:13 +0530

    validateStates-conditions | ongoing

--- test/script.js
+++ test/script.js
@@ -1,3 +1,20 @@
 const myformValidator = require("form-validator");
 
-console.log(myformValidator());
+const validationRules = {
+    name: {
+        value: "swalih",
+        required: true,
+        type: "text",
+        minLength: 4,
+        maxLength: 10,
+    },
+    pwd: {
+        value: "Swa",
+        required: true,
+        type: "password",
+        minLength: 4,
+        maxLength: 10,
+    },
+};
+
+console.log("-------", myformValidator(validationRules));
